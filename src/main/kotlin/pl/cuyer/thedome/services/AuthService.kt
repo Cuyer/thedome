8b9f6fb3e0fa214fc6bf64dc17471eecbd342e74
Krystian Checinski <krystixn.checinski@gmail.com>
2025-06-30 15:20:09 +0200

    fix

--- src/main/kotlin/pl/cuyer/thedome/services/AuthService.kt
+++ src/main/kotlin/pl/cuyer/thedome/services/AuthService.kt
@@ -25,7 +25,6 @@ class AuthService(
     private val tokenValidityMs: Long,
     private val anonTokenValidityMs: Long,
     private val tokenService: FcmTokenService
-    private val fcmTokenService: FcmTokenService
 ) {
     private val algorithm = Algorithm.HMAC256(jwtSecret)
     private val logger = LoggerFactory.getLogger(AuthService::class.java)
@@ -80,7 +79,7 @@ class AuthService(
         val refresh = generateRefreshToken()
         val hashedRefresh = hashToken(refresh)
         collection.updateOne(eq(User::username, user.username), set(User::refreshToken, hashedRefresh))
-        fcmTokenService.resubscribeUserTokens(user.username)
+        tokenService.resubscribeUserTokens(user.username)
         logger.info("User ${user.username} logged in")
         return TokenPair(generateAccessToken(user, tokenValidityMs), refresh, user.username, user.email)
     }
